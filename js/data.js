b921e4fb3f8c7fd4e69f405bf40151365fb230c5
TheLegendaryCro <you@example.com>
2019-08-09 14:19:44 -0500

    Upload

---
+++ js/data.js
@@ -0,0 +1,108 @@
+var headerText = "Get To Know You Bingo";
+
+var footerText = "<a href='https://github.com/TheLegendaryCro' target='_blank'>My Github</a>";
+
+var winText = "Winner";
+
+var JSONBingo = {
+  "squares": [{
+      "square": "likes cats"
+    },
+    {
+      "square": "likes dogs"
+    },
+    {
+      "square": "loves to watch sports"
+    },
+    {
+      "square": "uses instagram"
+    },
+    {
+      "square": "likes to go hiking"
+    },
+    {
+      "square": "plays video games"
+    },
+    {
+      "square": "has freckles"
+    },
+    {
+      "square": "went on a blind date"
+    },
+    {
+      "square": "is the youngest child"
+    },
+    {
+      "square": "is the oldest child"
+    },
+    {
+      "square": "has votes in an election"
+    },
+    {
+      "square": "has caught a fish"
+    },
+    {
+      "square": "has gone to europe"
+    },
+    {
+      "square": "has gone to mexico"
+    },
+    {
+      "square": "likes to dance"
+    },
+    {
+      "square": "finished college"
+    },
+    {
+      "square": "owns a pool"
+    },
+    {
+      "square": "has a twitter"
+    },
+    {
+      "square": "goes to the gym"
+    },
+    {
+      "square": "wears glasses or contacts"
+    },
+    {
+      "square": "doesn't know how to swim"
+    },
+    {
+      "square": "is left-handed"
+    },
+    {
+      "square": "does not drink soda"
+    },
+    {
+      "square": "cuts their own hair"
+    },
+    {
+      "square": "plays a musical instrument"
+    },
+    {
+      "square": "is a live streamer or youtuber"
+    },
+    {
+      "square": "never finished high school"
+    },
+    {
+      "square": "likes marvel"
+    },
+    {
+      "square": "is left-handed"
+    },
+    {
+      "square": "has traveled to more than 5 states"
+    },
+    {
+      "square": "enjoys reading books"
+    },
+    {
+      "square": "can ride a skateboard"
+    },
+    {
+      "square": "has build their own computer"
+    }
+  ]
+};
